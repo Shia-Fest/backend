1ad876f66526a42afb5e98751748813234dfc8ab
ajmalMundumuzhi <muhajmal73@gmail.com>
2025-09-12 10:53:41 +0530

    feat: initialize Node.js express server with MVC structure

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
